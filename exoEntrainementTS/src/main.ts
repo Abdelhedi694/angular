37f00b6fced31f0c9366c753cdb348d0aa276be2
Abdelhedi Rafaï <abdel.hedi69400@gmail.com>
2021-11-23 11:23:46 +0100

    ajout fonction qui ajoute dans ul li un seul jeu

--- exoEntrainementTS/src/main.ts
+++ exoEntrainementTS/src/main.ts
@@ -1,6 +1,40 @@
 import {tableauJeux} from "./creationTableauEnDur";
-console.log(tableauJeux);
-
 import "./style.css"
 
 
+type jeux = {
+    nom : string
+    genre : string
+    editeur : string
+    pegi : number
+    anneeSortie : number
+    multijoueur : boolean
+    enLigne : boolean
+    image : URL
+}
+
+function ajoutUnJeux(jeux:jeux) {
+    let html:string = ``
+    html+= `<ul>`
+    
+        html+= `<li>${jeux.nom}</li>
+        <li>${jeux.genre}</li>
+        <li>${jeux.editeur}</li>
+        <li>${jeux.pegi}</li>
+        <li>${jeux.anneeSortie}</li>
+        <li>${jeux.multijoueur}</li>
+        <li>${jeux.enLigne}</li>
+        <li>${jeux.image}</li>`
+    
+    html+= `</ul>`
+    console.log(html);
+    
+    
+}
+ajoutUnJeux(tableauJeux[1])
+
+
+document.querySelector("form")?.addEventListener("submit",function (e:Event) {
+    e.preventDefault()
+
+})
