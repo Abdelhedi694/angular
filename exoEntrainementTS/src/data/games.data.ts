8a4e89658f25ceee31d121e4a029a6ad0d6ce45a
Abdelhedi Rafaï <abdel.hedi69400@gmail.com>
2021-11-24 09:24:23 +0100

    création dossier data contenant les données (class, tableau)

---
+++ exoEntrainementTS/src/data/games.data.ts
@@ -0,0 +1,6 @@
+import { jeux } from "../creationTableauEnDur"
+export const tableauJeux:jeux[] = [
+
+    new jeux("oddworld", "aventure", "jeuxConsole", 11, 1996, false, false, new URL("https://www.numerama.com/wp-content/uploads/2021/04/capture-decran-2021-04-09-a-16-41-02.jpg") ),
+    new jeux("medievil", "horeur", "konami", 18, 1992, false, false, new URL("https://d3isma7snj3lcx.cloudfront.net/optim/images/news/30/3050819933/le-remake-de-medievil-se-devoile-en-compagnie-de-ses-restaurateurs-085a6bfc__w854.jpg") ),
+]
