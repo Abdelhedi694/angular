5e347e12593870bdccad49791815dba925e45813
Abdelhedi Rafaï <abdel.hedi69400@gmail.com>
2021-11-24 09:20:06 +0100

    modification de type jeu remplacé par une classe jeu et suppression du tableau a cet endroit

--- exoEntrainementTS/src/creationTableauEnDur.ts
+++ exoEntrainementTS/src/creationTableauEnDur.ts
@@ -1,40 +1,28 @@
 
+export class jeux{
+   public nom : string;
+   public genre : string;
+   public editeur : string;
+   public pegi : number;
+   public anneeSortie : number;
+   public multijoueur : boolean;
+   public enLigne : boolean;
+   public image : URL;
 
-
-
-type jeux = {
-    nom : string
-    genre : string
-    editeur : string
-    pegi : number
-    anneeSortie : number
-    multijoueur : boolean
-    enLigne : boolean
-    image : URL
+   constructor(nom:string, genre:string, editeur:string, pegi:number, anneeSortie:number,multijoueur:boolean, enLigne:boolean, image:URL){
+       this.nom = nom;
+       this.genre = genre;
+       this.editeur = editeur;
+       this.pegi = pegi;
+       this.anneeSortie = anneeSortie;
+       this.multijoueur = multijoueur;
+       this.enLigne = enLigne;
+       this.image = image
+   }
 }
 
-let tableauJeux:jeux[] = [
-    {
-        nom : "oddworld",
-        genre : "aventure",
-        editeur : "jeuxConsole",
-        pegi : 11,
-        anneeSortie : 1996,
-        multijoueur : false,
-        enLigne : false,
-        image : new URL("https://www.numerama.com/wp-content/uploads/2021/04/capture-decran-2021-04-09-a-16-41-02.jpg")   
-    },
-    {
-        nom : "medievil",
-        genre : "horeur",
-        editeur : "konami",
-        pegi : 18,
-        anneeSortie : 1992,
-        multijoueur : false,
-        enLigne : false,
-        image : new URL("https://d3isma7snj3lcx.cloudfront.net/optim/images/news/30/3050819933/le-remake-de-medievil-se-devoile-en-compagnie-de-ses-restaurateurs-085a6bfc__w854.jpg")   
-    }
-]
 
 
-export { tableauJeux } ;
+
+
+
